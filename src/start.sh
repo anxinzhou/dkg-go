879755d668cba75e643927468b3499c8047902f9
anxin <anxin@anxins-MacBook-Pro.local>
2019-01-26 16:04:55 +0800

    run script

---
+++ src/start.sh
@@ -0,0 +1,4 @@
+host=$(/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
+port=4000
+export GOPATH=$(dirname $(pwd))
+go run main.go -host=$host -port=$port -p=1
