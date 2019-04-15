e2e867157cb57aed5fa480104211d93372ab4916
anxin <anxin@anxins-MacBook-Pro.local>
2019-04-15 11:35:00 +0800

    config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-pssh -i -x "-o StrictHostKeyChecking=no" -h server.json "git clone https://github.com/xxRanger/dkg-go.git"
+pssh -i -x "-o StrictHostKeyChecking=no" -h server.json "git clone https://github.com/xxRanger/dkg-go.git && sudo snap install go --classic"
